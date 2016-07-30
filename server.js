8a2a42d3db9f8783fbb477fb3b31c0d82a586cef
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2016-07-29 21:49:03 -0300

    Correct server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
+    host: '64.137.223.64',
     port: 8000
 });
 
