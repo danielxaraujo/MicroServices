c20b41eabc0bc3a9de69178ffc4f32015e32b0fa
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2016-07-29 22:16:12 -0300

    Testando outra rota

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ server.route({
     }
 });
 
+server.route({
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: function(request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
 // Start the server
 server.start((err) => {
     if (err) {
